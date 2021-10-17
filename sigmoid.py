92c9e0cba4ca67334d9c19a3d5a82fb412dcecd5
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2021-10-17 15:29:25 +0900

    [버그]: 오타 수정 완료 s자 모양 출력 완료

--- sigmoid.py
+++ sigmoid.py
@@ -4,7 +4,7 @@ import matplotlib.pylab as plt
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-x = np.arange(-0.5, 5.0, 0.1)
+x = np.arange(-5.0, 5.0, 0.1)
 y = sigmoid(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
