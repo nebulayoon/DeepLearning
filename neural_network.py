a70529375ba5286566559ce6f68f75a7d13229d9
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2021-11-13 23:13:06 +0900

    [기능추가]: 출력층 활성함수(항등함수) 추가

--- neural_network.py
+++ neural_network.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigmoid(a1):
     return 1 / (1 + np.exp(a1))
 
+def identity_function(x): #항등함수
+    return x
+
 X = np.array([1, 2])
 
 W = np.array([[1, 3, 5], [2, 4, 6]])
@@ -27,3 +30,10 @@ z2 = sigmoid(a2)
 
 print(a2)
 print(z2)
+
+w3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+b3 = np.array([0.1, 0.2])
+
+a3 = np.dot(z2, w3) + b3
+y = identity_function(a3)
+print(y)
