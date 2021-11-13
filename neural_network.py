9c97cc61f5267d069e3cf511383f500ad30f3ec4
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2021-11-13 23:06:56 +0900

    [기능추가]: 신경망 2층 구현

---
+++ neural_network.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def sigmoid(a1):
+    return 1 / (1 + np.exp(a1))
+
+X = np.array([1, 2])
+
+W = np.array([[1, 3, 5], [2, 4, 6]])
+
+Y = np.dot(X, W)
+print(Y)
+
+x = np.array([1.0, 0.5])
+w1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+b1 = np.array([0.1, 0.2, 0.3])
+
+a1 = np.dot(x, w1) + b1
+print(a1)
+z1 = sigmoid(a1)
+print(z1)
+
+w2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+b2 = np.array([0.1, 0.2])
+
+a2 = np.dot(z1, w2) + b2
+z2 = sigmoid(a2)
+
+print(a2)
+print(z2)
--- relu.py
+++ relu.py
@@ -13,3 +13,16 @@ print(A.shape[0])
 B = np.array([[1, 2], [3, 4], [5, 6]])
 print(np.ndim(B))
 print(B.shape)
+
+C = np.array([[1,2], [3, 4]])
+D = np.array([[5, 6], [7, 8]])
+E = np.dot(C, D) # 행렬의 곱
+print(E)
+
+print(np.dot(A, [5, 6, 7, 8]))
+
+# 당연하지만, 행렬곱은 A, B와 B, A의 값이 달라질수 있다.
+
+F = np.array([[1, 2, 3], [4, 5, 6]])
+G = np.array([[1, 2], [3, 4], [5, 6]])
+print(np.dot(F, G))
