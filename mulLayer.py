0f7a1a9efe75860b5488e7e5c0cdd877964db2c4
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2022-01-09 21:35:03 +0900

    [기타]: 사과 농장 예시 순전파 구현

--- mulLayer.py
+++ mulLayer.py
@@ -15,3 +15,15 @@ class MulLayer:
     dy = dout * self.x
     
     return dx, dy
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+
+print(price)
