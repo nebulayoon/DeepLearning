9fd5e255494af3886f0a51f1088f2f118f9e38d6
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2022-01-11 23:20:31 +0900

    [기타]: 백업

--- layers.py
+++ layers.py
@@ -34,3 +34,24 @@ class Sigmoid:
     dx = dout * (1.0 - self.out) * self.out
 
     return dx
+
+class Affine: # 추가적인 이해필요
+  def __init__(self, W, b):
+    self.W = W
+    self.b = b
+    self.x = None
+    self.dW = None
+    self.db = None
+
+  def forward(self, x):
+    self.x = x
+    out = np.dot(x, self.W) + self.b
+    
+    return out
+
+  def backward(self, dout):
+    dx = np.dot(dout, self.W.T)
+    self.dW = np.dot(self.x.T, dout)
+    self.db = np.sum(dout, axis=0)
+
+    return dx
