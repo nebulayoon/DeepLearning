154126a0c78962adad1f6f16d3033f4ce8fe2da2
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2022-01-13 12:23:12 +0900

    [기타]: softmax, cross entropy error 구현

--- layers.py
+++ layers.py
@@ -1,6 +1,22 @@
 # 이 프로젝트에서는 앞으로의 신경망 공부에서 사용될 여러 계층들을 구현할 예정임
 import numpy as np
 
+def softmax(a):
+  c = np.max(a)
+  exp_a = np.exp(a - c)
+  sum_exp_a = np.sum(exp_a)
+  y = exp_a / sum_exp_a
+
+  return y
+
+def cross_entropy_error(y, t):
+  if y.ndim == 1:
+    t = t.reshape(1, t.size)
+    y = y.reshape(1, y.size)
+
+  batch_size = y.shape[0]
+  return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
 class Relu:
   def __init__(self):
     self.mask = None
@@ -53,5 +69,24 @@ class Affine: # 추가적인 이해필요
     dx = np.dot(dout, self.W.T)
     self.dW = np.dot(self.x.T, dout)
     self.db = np.sum(dout, axis=0)
+
+    return dx
+
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None
+    self.t = None
+  
+  def forward(self, x, t):
+    self.t = t
+    self.y = softmax(x)
+    self.loss = cross_entropy_error(self.y, self.t)
+
+    return self.loss
+
+  def backward(self, dout = 1):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
 
     return dx
